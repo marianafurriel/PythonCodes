1a9f8351d21a7bad8eab7600c79ea067d17dc860
Mariana Furriel <marianafurrielss@gmail.com>
2023-01-11 13:44:25 -0300

    download do arquivo adicionado

--- automatizador_de_email.py
+++ automatizador_de_email.py
@@ -1,13 +1,25 @@
 import pyautogui as pag
 import time
 import pyperclip
-import pandas as pd
+
+# time.sleep(5)
+# print(pag.position())
 
 pag.PAUSE = 1
-pag.press('win')
-pag.write('opera')
+pag.press('win') #abre o menu windows
+pag.write('opera') #procura o navegador
+pag.press('enter') #abre o navegador
+pyperclip.copy("https://drive.google.com/drive/folders/14oLE59U1RqyRqlBbKpsyymW-mitvbtoh")
+pag.hotkey("ctrl","v")
 pag.press('enter')
-pag.write("https://drive.google.com/drive/folders/14oLE59U1RqyRqlBbKpsyymW-mitvbtoh")
+time.sleep(5) #espera a pagina carregar
+pag.click(x=-1298, y=340) #seleciona o documento
+pag.click(x=-256, y=241) #clica nos 3 pontos
+pag.click(x=-498, y=786) #clica em download
+time.sleep(2)
+pag.click(x=-1266, y=69) #clica na barra de endereço do explorador de arquivos
+pag.hotkey('ctrl','c') #copia o endereço onde vai salvar o arquivo
 pag.press('enter')
-#time.sleep(5)
-pag.doubleClick(x=-1433, y=356)
+pag.press('enter')
+caminho = pyperclip.paste()
+print(caminho)
