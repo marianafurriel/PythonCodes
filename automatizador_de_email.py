b06bc58b48a9ae4517b7e610de55eceecad8c6ee
Mariana Furriel <marianafurrielss@gmail.com>
2023-01-19 13:23:19 -0300

    Download completo

--- automatizador_de_email.py
+++ automatizador_de_email.py
@@ -9,15 +9,16 @@ pag.PAUSE = 1
 pag.press('win') #abre o menu windows
 pag.write('opera') #procura o navegador
 pag.press('enter') #abre o navegador
+pag.click(x=-1428, y=67)
 pyperclip.copy("https://drive.google.com/drive/folders/14oLE59U1RqyRqlBbKpsyymW-mitvbtoh")
 pag.hotkey("ctrl","v")
 pag.press('enter')
 time.sleep(5) #espera a pagina carregar
 pag.click(x=-1298, y=340) #seleciona o documento
-pag.click(x=-256, y=241) #clica nos 3 pontos
-pag.click(x=-498, y=786) #clica em download
+pag.click(x=-258, y=190) #clica nos 3 pontos
+pag.click(x=-554, y=708) #clica em download
 time.sleep(2)
-pag.click(x=-1266, y=69) #clica na barra de endereço do explorador de arquivos
+pag.click(x=-1275, y=60) #clica na barra de endereço do explorador de arquivos
 pag.hotkey('ctrl','c') #copia o endereço onde vai salvar o arquivo
 pag.press('enter')
 pag.press('enter')
