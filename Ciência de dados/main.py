daa4c714974d98658ff413918975dd421f3d9ab0
Mariana Furriel <marianafurrielss@gmail.com>
2023-01-23 11:26:10 -0300

    inteligencia artificial criada e treinada

--- Ciência de dados/main.py
+++ Ciência de dados/main.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split 
+#importar a inteligencia artificial
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 
-tabela = pd.read_csv('advertising.csv')
+tabela = pd.read_csv('advertising.csv') #importar base de dados
 
 sns.heatmap(tabela.corr(),cmap='Greens',annot=True) #cria o gráfico
 plt.show() #exibe o gráfico
@@ -11,5 +14,12 @@ plt.show() #exibe o gráfico
 y = tabela["Vendas"]
 x = tabela[["TV",'Radio','Jornal']]
 
-x_treino,x_teste,y_treino,y_teste = train_test_split(x,y) #separa nossa base de dados em dados de treino e dados de teste
+x_treino,x_teste,y_treino,y_teste = train_test_split(x,y,test_size=0.3) #separa nossa base de dados em dados de treino e dados de teste
 
+#criar a inteligencia artificial
+modelo_regressaolinear = LinearRegression()
+modelo_arvoredecisao = RandomForestRegressor()
+
+#treinar a inteligencia artificial
+modelo_regressaolinear.fit(x_treino,y_treino)
+modelo_arvoredecisao.fit(x_treino,y_treino)
